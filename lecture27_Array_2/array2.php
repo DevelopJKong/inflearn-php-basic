6fa55762bf1524266cb740407f2ee38fa2b552a0
DevelopJKong <ybaram40@gmail.com>
2021-10-18 22:46:01 +0900

    Array_2 완료

---
+++ lecture27_Array_2/array2.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Sort by value
+ */
+
+$numbers = range(1,5);
+
+//sort는 오름차순으로 정렬을 해준다
+//sort($numbers);
+
+//rsort는 내림차순으로 정렬을 해준다
+rsort($numbers);
+
+var_dump($numbers);
+
+ 
+/**
+ * Assoc sort by value
+ */
+$arr = [
+    'r' => 'Bye',
+    'c' => 'Who are you?',
+    'a' => 'Hello, world'
+];
+//연관 배열 정렬은 a를 붙여주면 된다
+
+arsort($arr);
+var_dump($arr);
+
+/**
+ * Sort by key
+ */
+
+//ksort($arr);
+krsort($arr);
+var_dump($arr);
+
+/**
+ * Sort by callback
+ */
+/************************************************************ */
+//usort vs uasort vs uksort 값을 가지고 정렬을 하는건데 두개의 차이를 정확하게 알아두어야 할거 같다
+/************************************************************ */
+usort($numbers,function($a,$b) {
+    return $a > $b;
+});
+
+var_dump($numbers);
+
+
+
+
